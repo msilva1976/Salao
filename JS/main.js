df327c2c4e702d9dd647ada61c816866819c1268
Marcos Carlos <marcosspom@gmail.com>
2022-12-10 20:42:53 -0300

    contprojsal02

---
+++ JS/main.js
@@ -0,0 +1,52 @@
+//abre e fecha o menu quando clicar no icone
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle){
+  element.addEventListener('click',function(){
+nav.classList.toggle('show')
+  })
+  }
+
+//quando clicar em um item do menu, esconder o menu
+
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links){
+  link.addEventListener('click', function () {    
+ nav.classList.remove('show')
+})
+}
+
+/*mudar o header da pagina quando der scroll*/
+const header = document.querySelectorAll('#header')
+const navHeight = header.offsetHeight
+window.addEventListener('scroll', function () {
+  if(window.scrollY >= navHeight){
+    //maior que a altura do header
+    header,classList.add('scroll')
+    
+}else{
+    //menor que a altura do header
+    header.classList.remove('scroll')
+     
+}
+})
+
+
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+   slidesperView:1,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+mousewheel: true,
+keyboard:true
+  
+  
+    
+  });
+  
